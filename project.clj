065dbcf277f38303656834edf25ea5ea3d09ebb0
Matt Revelle <matt@dynamictyping.org>
2010-11-01 01:35:45 -0400

    Removed old daemon code, updated http.async.client version.

--- project.clj
+++ project.clj
@@ -2,19 +2,12 @@
   :description "work based fetcher service."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [http.async.client "0.2.0"]
+                 [http.async.client "0.2.1"]
                  [javax.mail/mail "1.4.3"]
                  [work "0.0.1-SNAPSHOT"]
                  [crane "1.0-SNAPSHOT"]
-                 [org.clojars.mattrepl/lein-daemon "0.2.1-SNAPSHOT"]]
+                 [clj-time "0.1.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [crane/lein-crane "0.0.1-SNAPSHOT"]
-                     [org.clojars.mattrepl/lein-daemon "0.2.1-SNAPSHOT"]]
+                     [crane/lein-crane "0.0.1-SNAPSHOT"]]
   :repositories {"java.net" "http://download.java.net/maven/2"
-                 "clojars" "http://clojars.org/repo"}
-  :daemon {"aws-poller-scheduler" {:ns "aws-poller-scheduler-daemon"
-                                   :args []
-                                   :options {:pidfile "aws-poller-scheduler.pid"
-                                             :errfile "err.out"
-                                             :user "ubuntu"}
-                                   :extra-classpath ["crane"]}})
+                 "clojars" "http://clojars.org/repo"})
