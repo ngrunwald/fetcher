3fa9ed90be376daebbbb8f78397c764fcd71a7a8
Bradford <bradford@measuringmeasures.com>
2011-03-11 22:03:16 -0800

    decomposing the req-resp-client coupling and wrap-blah pipeline.

--- test/fetcher/client_test.clj
+++ test/fetcher/client_test.clj
@@ -1,8 +1,7 @@
 (ns fetcher.client-test
   (:use clojure.test
 	ring.adapter.jetty
-	ring.middleware.reload
-	fetcher.core-test)
+	ring.middleware.reload)
   (:use [plumbing.streams :only [test-stream]])
   (:require [fetcher.client :as client]
 	    [fetcher.util :as util]
@@ -10,8 +9,13 @@
   (:import (java.util Arrays)
 	   (java.util.concurrent.atomic AtomicInteger)))
 
+(def base-req
+  {:scheme "http"
+   :server-name "localhost"
+   :server-port 8080})
+
 (deftest rountrip
-  (let [resp (client/request (merge base-req {:uri "/get" :method :get}))]
+  (let [resp (client/request :get (merge base-req {:uri "/get"}))]
     (is (= 200 (:status resp)))
     (is (= "close" (get-in resp [:headers "connection"])))
     (is (= "get" (:body resp)))))
@@ -31,8 +35,9 @@
                     :headers {"location" "http://bar.com/bat"}}
                    {:status 200
                     :req req}))
-        r-client (client/wrap-redirects client)
-        resp (r-client {:server-name "foo.com" :request-method :get})]
+	req {:server-name "foo.com" :request-method :get}
+	first-resp (client req)
+        resp (client/redirect client req first-resp)]
     (is (= 200 (:status resp)))
     (is (= :get (:request-method (:req resp))))
     (is (= "http" (:scheme (:req resp))))
@@ -45,8 +50,9 @@
                     :headers {"location" "http://bar.com/bat"}}
                    {:status 200
                     :req req}))
-        r-client (client/wrap-redirects client)
-        resp (r-client {:server-name "foo.com" :request-method :head})]
+	req {:server-name "foo.com" :request-method :head}
+	first-resp (client req)
+        resp (client/redirect client req first-resp)]
     (is (= 200 (:status resp)))
     (is (= :get (:request-method (:req resp))))
     (is (= "http" (:scheme (:req resp))))
@@ -54,169 +60,118 @@
 
 (deftest pass-on-non-redirect
   (let [client (fn [req] {:status 200 :body (:body req)})
-        r-client (client/wrap-redirects client)
-        resp (r-client {:body "ok"})]
+	req {:body "ok"}
+	first-resp (client req)
+	resp (client/redirect client req first-resp)]
     (is (= 200 (:status resp)))
     (is (= "ok" (:body resp)))))
 
-
-(deftest throw-on-exceptional
-  (let [client (fn [req] {:status 500 :body "No worky."})
-        e-client (client/wrap-exceptions client)]
-    (is (thrown-with-msg? Exception #"500"
-	  (e-client {})))))
-
-(deftest pass-on-non-exceptional
-  (let [client (fn [req] {:status 200})
-        e-client (client/wrap-exceptions client)
-        resp (e-client {})]
-    (is (= 200 (:status resp)))))
-
-(deftest pass-on-exceptional-when-surpressed
-  (let [client (fn [req] {:status 500})
-        e-client (client/wrap-exceptions client)
-        resp (e-client {:throw-exceptions false})]
-    (is (= 500 (:status resp)))))
-
-
 (deftest apply-on-compressed
-  (let [client (fn [req] {:body (-> "foofoofoo"
+  (let [resp (client/decompress {:body (-> "foofoofoo"
 				    .getBytes
 				    util/gzip
 				    java.io.ByteArrayInputStream.)
-                          :headers {"Content-Encoding" "gzip"}})
-        c-client (client/wrap-decompression client)
-        resp (c-client {})]
+                          :headers {"Content-Encoding" "gzip"}})]
     (is (= "foofoofoo" (-> resp :body  io/slurp*)))))
 
 (deftest apply-on-deflated
-  (let [client (fn [req] {:body (-> "barbarbar" .getBytes
+  (let [resp (client/decompress {:body (-> "barbarbar" .getBytes
 				    util/deflate
 				    java.io.ByteArrayInputStream.)
-                          :headers {"Content-Encoding" "deflate"}})
-        c-client (client/wrap-decompression client)
-        resp (c-client {})]
+                          :headers {"Content-Encoding" "deflate"}})]
     (is (= "barbarbar" (io/slurp* (:body resp))))))
 
 (deftest pass-on-non-compressed
-  (let [c-client (client/wrap-decompression (fn [req] {:body "foo"}))
-        resp (c-client {:uri "/foo"})]
+  (let [resp (client/decompress {:body "foo"})]
     (is (= "foo" (:body resp)))))
 
-
 (deftest apply-on-accept
-  (is-applied client/wrap-accept
-	      {:accept :json}
-	      {:headers {"Accept" "application/json"}}))
+  (is (=
+       {:headers {"Accept" "application/json"}}
+       (client/accept {:accept :json}))))
 
 (deftest pass-on-no-accept
-  (is-passed client/wrap-accept
-	     {:uri "/foo"}))
-
+  (let [req {:uri "/foo"}]
+    (is (= req (client/accept req)))))
 
 (deftest apply-on-accept-encoding
-  (is-applied client/wrap-accept-encoding
-	      {:accept-encoding [:identity :gzip]}
-	      {:headers {"Accept-Encoding" "identity, gzip"}}))
+  (is (=  {:headers {"Accept-Encoding" "identity, gzip"}}
+	  (client/accept-encoding
+	   {:accept-encoding [:identity :gzip]}))))
 
 (deftest pass-on-no-accept-encoding
-  (is-passed client/wrap-accept-encoding
-	     {:uri "/foo"}))
+  (let [req {:uri "/foo"}]
+    (is (= req (client/accept-encoding req)))))
 
 
 (deftest apply-on-output-coercion
-  (let [client (fn [req] {:body (io/input-stream (.getBytes "foo"))})
-        o-client (client/wrap-output-coercion client)
-        resp (o-client {:uri "/foo"})]
-    (is (= "foo" (:body resp)))))
+  (let [resp (client/output-coercion
+	      {:as :byte-array}
+	      {:body (io/input-stream (.getBytes "foo"))})]
+    (is (Arrays/equals (.getBytes "foo") (:body resp)))))
 
 (deftest pass-on-no-output-coercion
-  (let [client (fn [req] {:body nil})
-        o-client (client/wrap-output-coercion client)
-        resp (o-client {:uri "/foo"})]
+  (let [resp (client/output-coercion {} {:body nil})]
     (is (nil? (:body resp))))
-  (let [client (fn [req] {:body :thebytes})
-        o-client (client/wrap-output-coercion client)
-        resp (o-client {:uri "/foo" :as :byte-array})]
+  (let [resp (client/output-coercion
+	      {:as :byte-array}
+	      {:body :thebytes})]
     (is (= :thebytes (:body resp)))))
 
-
 (deftest apply-on-input-coercion
-  (let [i-client (client/wrap-input-coercion identity)
-        resp (i-client {:body "foo"})]
+  (let [resp (client/input-coercion {:body "foo"})]
     (is (= "UTF-8" (:character-encoding resp)))
     (is (Arrays/equals (util/utf8-bytes "foo") (:body resp)))))
 
 (deftest pass-on-no-input-coercion
-  (is-passed client/wrap-input-coercion
-	     {:body (util/utf8-bytes "foo")}))
-
+  (let [req {:body (util/utf8-bytes "foo")}]
+    (is (= req
+	   (client/input-coercion req))))
 
 (deftest apply-on-content-type
-  (is-applied client/wrap-content-type
-	      {:content-type :json}
-	      {:content-type "application/json"}))
+  (is (= {:content-type "application/json"}
+	 (client/content-type
+	  {:content-type :json}))))
 
 (deftest pass-on-no-content-type
-  (is-passed client/wrap-content-type
-	     {:uri "/foo"}))
-
+  (let [req {:uri "/foo"}]
+    (is (= req (client/content-type req)))))
 
 (deftest apply-on-query-params
-  (is-applied client/wrap-query-params
-	      {:query-params {"foo" "bar" "dir" "<<"}}
-	      {:query-string "foo=bar&dir=%3C%3C"}))
+  (is (= {:query-string "foo=bar&dir=%3C%3C"}
+	 (client/query-params
+	  {:query-params {"foo" "bar" "dir" "<<"}}))))
 
 (deftest pass-on-no-query-params
-  (is-passed client/wrap-query-params
-	     {:uri "/foo"}))
-
+  (let [req {:uri "/foo"}]
+    (is (= req
+	   (client/query-params req)))))
 
 (deftest apply-on-basic-auth
-  (is-applied client/wrap-basic-auth
-	      {:basic-auth ["Aladdin" "open sesame"]}
-	      {:headers {"Authorization" "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="}}))
+  (is (= {:headers {"Authorization" "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="}}
+	 (client/basic-auth
+	  {:basic-auth ["Aladdin" "open sesame"]}))))
 
 (deftest pass-on-no-basic-auth
-  (is-passed client/wrap-basic-auth
-	     {:uri "/foo"}))
-
-(deftest apply-on-method
-  (let [m-client (client/wrap-method identity)
-        echo (m-client {:key :val :method :post})]
-    (is (= :val (:key echo)))
-    (is (= :post (:request-method echo)))
-    (is (not (:method echo)))))
-
-(deftest pass-on-no-method
-  (let [m-client (client/wrap-method identity)
-        echo (m-client {:key :val})]
-    (is (= :val (:key echo)))
-    (is (not (:request-method echo)))))
-
+  (let [req {:uri "/foo"}]
+    (is (= req
+	   (client/basic-auth req)))))
 
 (deftest apply-on-url
-  (let [u-client (client/wrap-url identity)
-        resp (u-client {:url "http://google.com:8080/foo?bar=bat"})]
+  (let [resp (client/parse-url "http://google.com:8080/foo?bar=bat")]
     (is (= "http" (:scheme resp)))
     (is (= "google.com" (:server-name resp)))
     (is (= 8080 (:server-port resp)))
     (is (= "/foo" (:uri resp)))
     (is (= "bar=bat" (:query-string resp)))))
 
-(deftest pass-on-no-url
-  (let [u-client (client/wrap-url identity)
-        resp (u-client {:uri "/foo"})]
-    (is (= "/foo" (:uri resp)))))
-
 (deftest chunked-request-test
-  (let [client (fn [req]
-		 {:body (-> "1\r\na\r\n3\r\nfoo\r\n0\r\n\r\n"
+  (let [resp (client/output-coercion
+	      {:chunked? true}
+		  {:body (-> "1\r\na\r\n3\r\nfoo\r\n0\r\n\r\n"
 			    .getBytes
 			    io/input-stream)
-		  :headers {"transfer-encoding" "chunked"}})
-	o-client (client/wrap-output-coercion client)
-	resp (o-client {:chunked? true})]
+		  :headers {"transfer-encoding" "chunked"}})]
     (is (= ["a" "foo"] (:body resp)))))
 
 #_(deftest chunked-request-stress-test
@@ -251,13 +206,14 @@
 <title>Zemřel Pavel Vondruška, muzikant a jeden z 'Cimrmanů' - www.lidovky.cz</title>
 </head><body></body></html>"))))
 
-;;TODO: deal with this - the userlying sax parser for clojure breaks on the below html but the parser in webime.parser works fine.
-#_(deftest charset-test
-  (is (= "windows-1250"
-         (charset (dom "<!DOCTYPE html PUBLIC \"-//Lidovky//DTD HTML 4//EN\" \"http://g.lidovky.cz/dtd/n3_uni.dtd\">
-<html><head>
-<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1250\">
-<meta http-equiv=\"cache-control\" content=\"no-cache\">
-<meta name=\"robots\" content=\"all\">
-<title>Zemřel Pavel Vondruška, muzikant a jeden z 'Cimrmanů' - www.lidovky.cz</title>
-</head><body></body></html>")))))
+;; ;;TODO: deal with this - the userlying sax parser for clojure breaks on the below html but the parser in webime.parser works fine.
+;; #_(deftest charset-test
+;;   (is (= "windows-1250"
+;;          (charset (dom "<!DOCTYPE html PUBLIC \"-//Lidovky//DTD HTML 4//EN\" \"http://g.lidovky.cz/dtd/n3_uni.dtd\">
+;; <html><head>
+;; <meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1250\">
+;; <meta http-equiv=\"cache-control\" content=\"no-cache\">
+;; <meta name=\"robots\" content=\"all\">
+;; <title>Zemřel Pavel Vondruška, muzikant a jeden z 'Cimrmanů' - www.lidovky.cz</title>
+;; </head><body></body></html>")))))
+)
